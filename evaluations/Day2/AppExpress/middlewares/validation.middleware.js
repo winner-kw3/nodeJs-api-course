2aa7fa67b3b17b1ad7509364f67ced642b0726a1
Winner Isac KODJO <kodjowinnerisac@gmail.com>
2026-03-23 16:57:34 +0100

    Evaluation Jour 2 - Winner Isac KODJO

--- evaluations/Day2/AppExpress/middlewares/validation.middleware.js
+++ evaluations/Day2/AppExpress/middlewares/validation.middleware.js
@@ -1,15 +1,20 @@
+const { ZodError } = require('zod');
+
 const validate = (schema) => (req, res, next) => {
     try {
         schema.parse(req.body);
         next();
     } catch (e) {
-        return res.status(400).json({
-            success: false,
-            errors: e.errors.map(err => ({
-                champ: err.path.join('.'),
-                message: err.message
-            }))
-        });
+        if (e instanceof ZodError) {
+            return res.status(400).json({
+                success: false,
+                errors: e.issues.map(issue => ({
+                    champ: issue.path.join('.'),
+                    message: issue.message
+                }))
+            });
+        }
+        next(e);
     }
 };
 
